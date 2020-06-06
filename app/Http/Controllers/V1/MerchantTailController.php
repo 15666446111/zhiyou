21f245991b1c66efd461bb31ef7d9f005950d7d1
yuzifei <1213649307@qq.com>
2020-06-06 12:30:12 +0800

    机具详情接口

--- app/Http/Controllers/V1/MerchantTailController.php
+++ app/Http/Controllers/V1/MerchantTailController.php
@@ -14,13 +14,22 @@ class MerchantTailController extends Controller
      */
     public function getMerchantsTail(Request $request)
     {
+        try{
 
-        //参数 friends伙伴  count总  user用户
-        $Type = $request->Type;
-        // dd($Type);
-        $server = new \App\Http\Controllers\V1\ServersController($Type, $request->user);
+            //参数 friends伙伴  count总  user用户
+            $Type = $request->Type;
+            // dd($Type);
+            $server = new \App\Http\Controllers\V1\ServersController($Type, $request->user);
 
-        $data = $server->getInfo();
+            $data = $server->getInfo();
+
+            return response()->json(['success'=>['message' => '获取成功!', 'data'=>$data]]); 
+
+        } catch (\Exception $e) {
+
+			return response()->json(['error'=>['message' => $e->getMessage()]]);
+		
+		}
 
     }
 }
--- app/Http/Controllers/V1/ServersController.php
+++ app/Http/Controllers/V1/ServersController.php
@@ -42,7 +42,7 @@ class ServersController extends Controller
         }else if($this->Type == "friends"){
     
             $this->users   = \App\BuserParent::where('parents', 'like', "%_".$this->Users->id."_%")->pluck('user_id')->toArray();
-            
+            // dd($this->users);
             $this->users[] = $this->Users->id;
             
         }else{
@@ -66,9 +66,11 @@ class ServersController extends Controller
         $arrs['AllMerchants'] = $this->getAllMerchants();
 
 
-        // $arrs['Bound'] = $this->getBound();
+        $arrs['Bound']        = $this->getBound();
 
-        // $arrs['UnBound'] = $this->getUnBound();
+        $arrs['UnBound']      = $this->getUnBound();
+
+        $arrs['Bind']         = $this->getBind();
 
 
 
@@ -82,12 +84,15 @@ class ServersController extends Controller
     {
         $users = $this->users;
 
-        $select = \App\Merchant::join('trades','merchants.merchant_sn','=','trades.merchant_sn')
-        ->whereIn('user_id',$users)
+        $select = \App\Trade::select('merchants.merchant_sn','merchants.bind_status','merchants.active_status','merchants.active_time','merchants.bind_time','trades.money','cashs.cash_type','cashs.cash_money')
+        ->join('merchants','merchants.merchant_sn','=','trades.merchant_sn')
+        ->join('cashs','cashs.order','=','trades.order')
+        ->whereIn('merchants.user_id',$users)
+        ->whereIn('cashs.user_id',$users)
         ->get()
         ->toArray();
         
-        dd($select);
+        return $select;
     }
     /**
      * 查询已绑定机器详情信息
@@ -97,13 +102,16 @@ class ServersController extends Controller
 
         $users = $this->users;
 
-        $select = \App\Merchant::join('trades','merchants.merchant_sn','=','trades.merchant_sn')
-        ->whereIn('user_id',$users)
+        $select = \App\Merchant::select('merchants.merchant_sn','merchants.bind_status','merchants.active_status','merchants.bind_time','trades.money','cashs.cash_type','cashs.cash_money')
+        ->join('trades','merchants.merchant_sn','=','trades.merchant_sn')
+        ->join('cashs','cashs.order','=','trades.order')
+        ->whereIn('merchants.user_id',$users)
+        ->whereIn('cashs.user_id',$users)
         ->where('bind_status',1)
         ->get()
         ->toArray();
         
-        dd($select);
+        return $select;
     }
 
     /**
@@ -113,12 +121,34 @@ class ServersController extends Controller
     {
         $users = $this->users;
 
-        $select = \App\Merchant::join('trades','merchants.merchant_sn','=','trades.merchant_sn')
-        ->whereIn('user_id',$users)
+        $select = \App\Merchant::select('merchants.merchant_sn','merchants.bind_status','merchants.active_status','trades.money','cashs.cash_type','cashs.cash_money')
+        ->join('trades','merchants.merchant_sn','=','trades.merchant_sn')
+        ->join('cashs','cashs.status','=','trades.is_cash')
+        ->whereIn('merchants.user_id',$users)
+        ->whereIn('cashs.user_id',$users)
         ->where('bind_status',0)
         ->get()
         ->toArray();
         
-        dd($select);
+        return $select;
+    }
+
+    /**
+     * 查询已激活机器详情
+     */
+    public function getBind()
+    {
+        $users = $this->users;
+
+        $select = \App\Merchant::select('merchants.merchant_sn','merchants.active_status','trades.money','cashs.cash_type','cashs.cash_money')
+        ->join('trades','merchants.merchant_sn','=','trades.merchant_sn')
+        ->join('cashs','cashs.status','=','trades.is_cash')
+        ->whereIn('merchants.user_id',$users)
+        ->whereIn('cashs.user_id',$users)
+        ->where('active_status',1)
+        ->get()
+        ->toArray();
+        
+        return $select;
     }
 }
