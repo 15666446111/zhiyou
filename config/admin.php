ff65737e8dd347f4f378e31643b71c5683767cc5
yuzifei <1213649307@qq.com>
2020-06-12 13:46:54 +0800

    优化后台logo

--- config/admin.php
+++ config/admin.php
@@ -250,7 +250,7 @@ return [
     | Set a default avatar for newly created users.
     |
     */
-    'default_avatar' => '/tests/xianglutong_log.png',
+    'default_avatar' => '/xianglutong_log.png',
     // 'default_avatar' => '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg',
 
     /*
