ac7182a8d912fb643d56356ad7b4d9fb614f1f04
Pudding <755969423@qq.com>
2020-06-22 16:39:58 +0800

    xiugai

--- app/Admin/Actions/HeadTailDeliverGoods.php
+++ app/Admin/Actions/HeadTailDeliverGoods.php
@@ -47,7 +47,7 @@ class HeadTailDeliverGoods extends Action
             }
 
 
-            \App\Merchant::whereIn('merchant_terminal', $data)->where('bind_status', '0')->whereNull('user_id')->update([
+            \App\Merchant::whereIn('merchant_terminal', $data)->where('bind_status', '0')->where('user_id', '0')->update([
                 'user_id'   =>  $request->user,
                 'policy_id' =>  $request->policy
             ]);
