49ed8f78d578b6f28e23a3e05e8c57587bb7dfe1
Pudding <755969423@qq.com>
2020-07-22 18:08:48 +0800

    忘记密码

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::prefix('V1')->group(function () {
      */
     Route::middleware('AuthToken')->get('/plug', 'V1\PlugController@index');        // 首页 - 轮播图
     Route::middleware('AuthToken')->get('/index_info', 'V1\IndexController@info');  // 首页 - 信息统计
-
+    Route::middleware('AuthToken')->get('/bonusPage', 'V1\BonusController@page');   // 首页 - 分红奖励
 
     /**
      * @author  [ Gong Ke] [< 755969423@qq.com >]
@@ -59,218 +59,89 @@ Route::prefix('V1')->group(function () {
     Route::middleware('AuthToken')->get('/notice',          'V1\ArticleController@Notice');         // 首页 - 系统公告
     Route::middleware('AuthToken')->get('/problem',         'V1\ArticleController@problem');        // 首页 - 常见问题
     Route::middleware('AuthToken')->get('/wx_share_list',   'V1\ArticleController@wxShare');        // 我的 - 微信分享文案
+    Route::middleware('AuthToken')->get('/getArticle',      'V1\ArticleController@Article');        // 获取文章详情
 
-
+    /**
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 首页 - 商户登记 >]
+     */
+    Route::middleware('AuthToken')->get('/getNoBindMerchant', 'V1\MerchantController@getNoBindList');        // 首页 - 商户登记 - 未绑定列表
+    Route::middleware('AuthToken')->get('/register',          'V1\MerchantsController@registers');           // 首页 - 商户登记 - 提交登记信息
 
 
     /**
-     * @version [<APP 首页 伙伴管理>] [<description>]
-     * @return  [首页的伙伴管理直接下级列表]   [<description>]
-     * @version [<伙伴管理] [<description>]
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 首页 - 商户管理  >]
      */
-    Route::middleware('AuthToken')->get('/my_team', 'V1\TeamController@index');
-
-
-    /**
-     * @version [<APP 我的栏位>] [<description>]
-     * @return  [个人信息 获取个人信息]   [<description>]
-     * @version [<个人信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/mine', 'V1\MineController@info');
-
-
-
-    /**
-     * @version [<APP 获取用户伙伴信息>] [<description>]
-     * @return  [个人信息 获取伙伴信息]   [<description>]
-     * @version [<伙伴信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->post('/userInfo', 'V1\MineController@userInfo');
-
-
-
-    /**
-     * @version [<APP 提现记录>] [<description>]
-     * @return  [个人信息 获取提现记录]   [<description>]
-     * @version [<提现记录信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/draw', 'V1\MineController@draw_log');
-
-
-
-    /**
-     * @version [<APP 获取消息通知>] [<description>]
-     * @return  [获取发送的消息接口]   [<description>]
-     * @version [<消息通知信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/message', 'V1\MessageController@getMessage');
-
-
-    /**
-     * @version [<APP 获取产品分类接口>] [<description>]
-     * @return  [获取正在展示的产品分类]   [<description>]
-     * @version [<产品分类信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getproducttype', 'V1\ProductController@getType');
-
-    /**
-     * @version [<APP 获取产品列表接口>] [<description>]
-     * @return  [获取正在展示的产品列表]   [<description>]
-     * @version [<产品列表信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getproduct', 'V1\ProductController@getProduct');
-
-    /**
-     * @version [<APP 获取产品信息接口>] [<description>]
-     * @return  [获取单独某个产品信息]   [<description>]
-     * @version [<产品信息接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getproductinfo', 'V1\ProductController@getProductInfo');
-
-
-    /**
-     * @version [<APP 商户登记>] [<description>]
-     * @return  [获取该用户下所有未登记的机器]   [<description>]
-     * @version [<未登记机器获取接口] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getNoBindMerchant', 'V1\MerchantController@getNoBindList');
-
-
-    /**
-     * @version [<vector>] [< 分红奖励页面 >]
-     */
-    Route::middleware('AuthToken')->get('/bonusPage', 'V1\BonusController@page');
-
-
-    /**
-     * @version [<APP 获取政策活动列表>] [<description>]
-     * @return  [获取平台所有的政策活动]   [<description>]
-     * @version [<获取政策后的] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getPolicy', 'V1\PolicyController@getPolicy');
-
-
-
-    /**
-     * @version [<APP 获取政策活动列表>] [<description>]
-     * @return  [获取平台所有的政策活动]   [<description>]
-     * @version [<获取政策后的] [<description>]
-     */
-    Route::middleware('AuthToken')->post('/getPolicyInfo', 'V1\PolicyController@getPolicyInfo');
-
-    /**
-     * @version [<APP 获取政策活动列表>] [<description>]
-     * @return  [获取平台所有的政策活动]   [<description>]
-     * @version [<获取政策后的] [<description>]
-     */
-    Route::middleware('AuthToken')->post('/setPolicyInfo', 'V1\PolicyController@setPolicyInfo');
-
-
-    /**
-     * @version [<APP 获取机具活动详情>] [<description>]
-     * @return  [用户发在朋友圈的推荐二维码 被朋友扫码申请后的信息]   [<description>]
-     * @version [< ] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/getApplyFirend', 'V1\ApplicationController@list');
-
-    /**
-     * @version [<APP 获取机具活动详情>] [<description>]
-     * @return  [用户发在朋友圈的推荐二维码 被朋友扫码申请后的信息]   [<description>]
-     * @version [< ] [<description>]
-     */
-    Route::middleware('AuthToken')->get('/setApplyFirend', 'V1\ApplicationController@set');
-
-   
-
-    /**
-     * 收益页面接口
-     */
-    Route::middleware('AuthToken')->get('/cashs', 'V1\CashsController@cashsIndex');
-
-    /**
-     * 首页商户登记绑定接口
-     */
-    Route::middleware('AuthToken')->get('/register', 'V1\MerchantsController@registers');
-
-    /**
-     * 修改个人登录密码接口
-     */
-    Route::middleware('AuthToken')->get('/setUserPwd', 'V1\SetUserController@updatePwd');
- 
-
-
-
-    /**
-     * 用户提现接口
-     */
-    Route::middleware('AuthToken')->post('/getWithdrawal', 'V1\SetUserController@Withdrawal');
-
-
-    /**
-     * 生成订单接口
-     */
-    Route::middleware('AuthToken')->post('/addOrderCreate', 'V1\OrdersController@orderCreate');
-
-
-    /**
-     * 查询订单接口
-     */
-    Route::middleware('AuthToken')->get('/getOrderUser', 'V1\OrdersController@getOrder');
-
-
-    /**
-     * 机具管理页面接口
-     */
-    Route::middleware('AuthToken')->get('/getBindAll', 'V1\MerchantController@getBind');
-
-
-    /**
-     * 提现税点接口
-     */
-    Route::middleware('AuthToken')->get('/getPoint', 'V1\SetUserController@point');
-
-
-    /**
-     * 文章详情接口
-     */
-    Route::middleware('AuthToken')->get('/getArticle', 'V1\ArticleController@Article');
-
-
-    /**
-     * 机具详情接口
-     */
-    Route::middleware('AuthToken')->get('/getTail', 'V1\MerchantTailController@getMerchantsTail');
-
-    
-    /**
-     * 查询用户未绑定终端机器
-     */
-    Route::middleware('AuthToken')->get('/getUnBoundInfo', 'V1\TransferController@getUnBound');
+    Route::middleware('AuthToken')->get('/getMerchantsList',   'V1\MerchantsController@merchantsList');         // 商户管理 - 商户列表
+    Route::middleware('AuthToken')->get('/getMerchantsInfo',   'V1\MerchantsController@merchantInfo');          // 商户管理 - 商户详情
+    Route::middleware('AuthToken')->get('/getMerchantsPolicy', 'V1\MerchantsController@merchantPolicy');        // 商户管理 - 商户详情 - 活动信息
+    Route::middleware('AuthToken')->get('/getMerchantsDetails','V1\MerchantsController@MerchantDetails');       // 商户管理 - 商户详情 - 交易明细
     
 
     /**
-     * 划拨
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 首页 - 伙伴管理  >]
      */
-    Route::middleware('AuthToken')->post('/addTransfer', 'V1\TransferController@transfer');
+    Route::middleware('AuthToken')->get('/my_team', 'V1\TeamController@index');                                 // 首页 - 伙伴管理 - 伙伴列表
+    Route::middleware('AuthToken')->post('/userInfo', 'V1\MineController@userInfo');                            // 首页 - 伙伴管理 - 伙伴详情
+    Route::middleware('AuthToken')->post('/getPolicyInfo', 'V1\PolicyController@getPolicyInfo');                // 首页 - 伙伴管理 - 政策列表
+    Route::middleware('AuthToken')->post('/setPolicyInfo', 'V1\PolicyController@setPolicyInfo');                // 首页 - 伙伴管理 - 设置政策
+
+
 
 
     /**
-     * 回拨机器列表
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 我的 - 我的栏位  >]
      */
-    Route::middleware('AuthToken')->get('/getBackList', 'V1\TransferController@backList');
+    Route::middleware('AuthToken')->get('/mine', 'V1\MineController@info');                                     // 我的 - 获取个人信息
+    Route::middleware('AuthToken')->get('/message', 'V1\MessageController@getMessage');                         // 我的 - 我的消息列表
+    Route::middleware('AuthToken')->get('/setUserPwd', 'V1\SetUserController@updatePwd');                       // 我的 - 设置 - 修改个人密码
+
 
 
     /**
-     * 回拨
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 我的 - 提现相关  >]
      */
-    Route::middleware('AuthToken')->post('/addBackTransfer', 'V1\TransferController@backTransfer');
+    Route::middleware('AuthToken')->get('/getPoint', 'V1\SetUserController@point');                             // 我的 - 提现 - 获取提现税点
+    Route::middleware('AuthToken')->post('/getWithdrawal', 'V1\SetUserController@Withdrawal');                  // 我的 - 提现 - 申请提现
+    Route::middleware('AuthToken')->get('/draw', 'V1\MineController@draw_log');                                 // 我的 - 提现 - 提现记录列表
+
+
 
     /**
-     * @version [<vector>] [< 划拨回拨记录 >]
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 首页 - 商城  >]
      */
-    Route::middleware('AuthToken')->get('/getTransferLog', 'V1\TransferController@transferLog');
+    Route::middleware('AuthToken')->get('/getproducttype', 'V1\ProductController@getType');                     // 首页 - 商城 - 获取产品分类
+    Route::middleware('AuthToken')->get('/getproduct', 'V1\ProductController@getProduct');                      // 首页 - 商城 - 获取产品列表
+    Route::middleware('AuthToken')->get('/getproductinfo', 'V1\ProductController@getProductInfo');              // 首页 - 商城 - 获取产品详情
+    Route::middleware('AuthToken')->post('/addOrderCreate', 'V1\OrdersController@orderCreate');                 // 首页 - 商城 - 生成订单信息
+    Route::middleware('AuthToken')->get('/getOrderUser', 'V1\OrdersController@getOrder');                       // 我的 - 订单 - 获取订单信息
 
 
+    /**
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 我的 - 机具管理  >]
+     */
+    Route::middleware('AuthToken')->get('/getBindAll', 'V1\MerchantController@getBind');                        // 我的 - 机具管理 - 获取绑定的机具
+    Route::middleware('AuthToken')->get('/getTail', 'V1\MerchantTailController@getMerchantsTail');              // 我的 - 机具管理 - 获取机具详情
+    Route::middleware('AuthToken')->get('/getUnBoundInfo', 'V1\TransferController@getUnBound');                 // 我的 - 机具管理 - 未绑定的机具
+    Route::middleware('AuthToken')->post('/addTransfer', 'V1\TransferController@transfer');                     // 我的 - 机具管理 - 机具划拨
+    Route::middleware('AuthToken')->get('/getBackList', 'V1\TransferController@backList');                      // 我的 - 机具管理 - 机具回拨列表
+    Route::middleware('AuthToken')->post('/addBackTransfer', 'V1\TransferController@backTransfer');             // 我的 - 机具管理 - 机具回拨
+    Route::middleware('AuthToken')->get('/getTransferLog', 'V1\TransferController@transferLog');                // 我的 - 机具管理 - 划拨回拨记录
+    Route::middleware('AuthToken')->get('/getPolicy', 'V1\PolicyController@getPolicy');                         // 我的 - 机具管理 - 政策活动选择
+
+
+    /**
+     * @author  [ Gong Ke] [< 755969423@qq.com >]
+     * @version [<vector>] [< 我的 - 推广商户 - 获取我的推荐机具  >]
+     */
+    Route::middleware('AuthToken')->get('/getApplyFirend', 'V1\ApplicationController@list');                // 我的 - 商户推荐 - 获取推荐列表
+    Route::middleware('AuthToken')->get('/setApplyFirend', 'V1\ApplicationController@set');                 // 我的 - 商户推荐 - 设置推荐机具
 
 
     /**
@@ -299,13 +170,10 @@ Route::prefix('V1')->group(function () {
 
     /**
      * @author  [ Gong Ke] [< 755969423@qq.com >]
-     * @version [<vector>] [< 首页 - 商户管理  >]
+     * @version [<vector>] [<  收益 - 收益栏目  >]
      */
-    Route::middleware('AuthToken')->get('/getMerchantsList',   'V1\MerchantsController@merchantsList');         // 商户管理 - 商户列表
-    Route::middleware('AuthToken')->get('/getMerchantsInfo',   'V1\MerchantsController@merchantInfo');          // 商户管理 - 商户详情
-    Route::middleware('AuthToken')->get('/getMerchantsPolicy', 'V1\MerchantsController@merchantPolicy');        // 商户管理 - 商户详情 - 活动信息
-    Route::middleware('AuthToken')->get('/getMerchantsDetails','V1\MerchantsController@MerchantDetails');       // 商户管理 - 商户详情 - 交易明细
-    
+    Route::middleware('AuthToken')->get('/cashs', 'V1\CashsController@cashsIndex');                     // 收益信息
+
 
     /**
      * @author  [ Gong Ke] [< 755969423@qq.com >]
@@ -320,8 +188,8 @@ Route::prefix('V1')->group(function () {
     Route::middleware('AuthToken')->get('/getAgentTeam',       'V1\DetailController@TeamDetail');   // 团队-业务详情-团队数据
     //Route::middleware('AuthToken')->get('/getAgentMerchant',   'V1\DetailController@MercDetail');  // 团队-业务详情-商户数据
     Route::middleware('AuthToken')->get('/getAvgTemail',       'V1\DetailController@AvgDetail');    // 团队-业务详情-台均数据
-});
 
+});
 
 
 Route::fallback(function(){ 
